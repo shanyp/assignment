702e6ab5bb61c34d63bcf98c75d3264993d8bf27
Shany Pozin <shanyp@microsoft.com>
2020-05-10 01:47:45 +0300

    fix ssh

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
-mkdir -p /home/vagrant/.ssh
-echo "Host server2
-    HostName 192.168.100.11" >> /home/vagrant/.ssh/config
+echo "192.168.100.11 server2" >> /etc/hosts
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
-mkdir -p /home/vagrant/.ssh
-echo "Host server1
-    HostName 192.168.100.10" >> /home/vagrant/.ssh/config
+echo "192.168.100.10 server1" >> /etc/hosts
+
